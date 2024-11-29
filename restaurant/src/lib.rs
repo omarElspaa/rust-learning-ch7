c6947eb02a277a9eef34de0a2449b0a17480ee24
omarElspaa <elspaa.omar@gmail.com>
2024-11-29 20:38:00 +0200

    second commit

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -7,10 +7,26 @@ mod front_of_house {
 // without using pub keyword this compile-time error will arise "module `hosting` is private"
 // In Rust, all items (functions, methods, structs, enums, modules, and constants) are private to parent modules by default.
 // Items in a parent module can't use the private items inside child modules, but items in child modules can use the items in their ancestor modules. This is because child modules wrap and hide their implementation details, but the child modules can see the context in which they're defined.
+// If you plan on sharing your library crate so other projects can use your code, your public API is your contract with users of your crate that determines how they can interact with your code, There are many considerations around managing changes to your public API to make it easier for people to depend on your crate. These considerations are beyond the scope of this book; if you're interested in this topic, see the Rust API Guidelines at https://rust-lang.github.io/api-guidelines.
 
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+}
+
+
+
+// We can construct relative paths that begin in the parent module, rather than the current module or the crate root, by using super at the start of the path. This is like starting a filesystem path with the .. syntax.
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
